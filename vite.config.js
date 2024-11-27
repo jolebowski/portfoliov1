4a9531ef65b9057faf3103c6243881ed67b182fd
Jordan Moreira <jordan.moreira@epitech.eu>
2024-11-27 23:55:14 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('src/components/')) {
-            return 'components'
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        },
-      },
-      input: './src/main.jsx',
-    },
+    outDir: 'dist',
   },
   server: {
     port: 3000,
